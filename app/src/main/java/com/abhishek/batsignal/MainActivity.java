59ce2678bb5dd975e7a3f6dd67f8a0c9c89abd8c
Abhishek Satam <abhisheksatam17@gmail.com>
2019-01-28 08:24:44 +0530

    Run the core logic in on Resume() call

--- app/src/main/java/com/abhishek/batsignal/MainActivity.java
+++ app/src/main/java/com/abhishek/batsignal/MainActivity.java
@@ -4,9 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.CallLog;
 import android.support.annotation.NonNull;
@@ -14,13 +12,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     private Core core;
     private Cursor managedCursor;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +26,50 @@ public class MainActivity extends AppCompatActivity {
         Log.d("DEBUGTAG", "onCreate() called..");
 
         Log.d("DEBUGTAG", CallLog.Calls.CACHED_FORMATTED_NUMBER);
+
+        // request permissions at start if not granted already
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_CALL_LOG}, 1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("DEBUGTAG", "onResume() called..");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // don't proceed if permissions not granted
+            return;
+        }
+        //printCallLogs(managedCursor);
+
+        // TODO: find a way to initialise these objects only once; can't initialise the objects in onCreate() as onResume() is called before the permission grant dialogbox is resolved..
+        // TODO: make use of singleton pattern?
+        managedCursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, CallLog.Calls.DATE);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        core = new Core(managedCursor, vibrator);
+
+        core.run();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("DEBUGTAG", "onRequestPermissionsResult() called..");
+
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("DEBUGTAG", "Permission granted");
+//                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+//                        return;
+//                    }
+
+                } else
+                    Log.d("DEBUGTAG", "Permission denied");
+            }
+        }
     }
 
     private void printCallLogs(Cursor managedCursor) {
@@ -50,43 +88,5 @@ public class MainActivity extends AppCompatActivity {
         Log.d("last entry", managedCursor.getString(number));
         Log.d("last entry", managedCursor.getString(name));
         Log.d("last entry type", (Integer.parseInt(managedCursor.getString(type)) == CallLog.Calls.MISSED_TYPE) ? "missed" : "something else");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case 1: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("DEBUGTAG", "Permission granted");
-
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
-                    Cursor managedCursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, CallLog.Calls.DATE);
-                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-                    //printCallLogs(managedCursor);
-
-                    core = new Core(managedCursor, vibrator);
-                    core.run();
-                } else
-                    Log.d("DEBUGTAG", "Permission denied");
-            }
-
-        }
     }
 }
